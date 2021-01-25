717e3f86441daa750524f6494b8147bab4bcfeb4
Abayomi <eyitouyoa3@gmail.com>
2021-01-25 14:49:04 -0500

    setup react correctly this time LOL

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const Navbar = () => {
+
+    //Might want to remove the handleClick funtion on the navlinks
+    return (
+        <nav>
+            <div className="logo">
+                <h1><a href="/#home">Abayomi</a>
+                </h1>
+            </div>
+
+            <ul className="nav-items">
+                <li className="nav-item">
+                    <a href="/#services">Services</a>
+                </li>
+                <li className="nav-item">
+                    <a href="/#portfolio"> Portfolio</a>
+                </li>
+                <li className="nav-item">
+                    <a href="/#about">About</a>
+                </li>
+                <li className="nav-item">
+                    <a href="/#contact">Contact</a>
+                </li>
+            </ul>
+
+            <div className="burger">
+                <div className="line1"></div>
+                <div className="line2"></div>
+                <div className="line3"></div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
