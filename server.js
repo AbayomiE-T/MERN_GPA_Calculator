f19dfde00e20e13e8c5d604b1d04bae3cd71384c
Abayomi <eyitouyoa3@gmail.com>
2021-01-31 12:01:42 -0500

    ready to deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const config = require('config');
+const path = require('path');
 
 const courses = require('./routes/api/courses');
 const users = require('./routes/api/users');
@@ -23,9 +24,20 @@ mongoose
     .then(() => console.log("MonogDB Connected..."))
     .catch((err) => console.log(err));
 
+//Use Routes
 app.use('/api/courses', courses);
 app.use('/api/users', users);
 app.use('/api/auth', auth);
+
+//Serve statis assets if in production
+if (process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    });
+}
 
 const port = process.env.PORT || 5000;
 
